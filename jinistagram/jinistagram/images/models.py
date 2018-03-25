ff58c7913be07bdedf1d6640e578fec47a2f482c
jiniC <jinic323@gmail.com>
2018-03-25 21:07:53 +0900

    Customizing the Django Admin

--- jinistagram/jinistagram/images/models.py
+++ jinistagram/jinistagram/images/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from jinistagram.users import models as user_models # User 불러오기
 
 # Create your models here.
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
     # 최초 생성
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,20 +13,32 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
     """ Image Model """
     file = models.ImageField()
     location = models.CharField(max_length=140)
-    cpation = models.TextField()
+    caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
     """ Comment Model """
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
 
+    def __str__(self):
+        return self.message
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
     """ Like Model """
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User:{} - Image Caption{}'.format(self.creator.username, self.image.caption)
