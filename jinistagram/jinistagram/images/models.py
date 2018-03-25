9d76e046b088ec03b78e840e81baa6485fc4580c
jiniC <jinic323@gmail.com>
2018-03-25 12:11:24 +0900

    Creating the Image Model & Abstract TimeStamp Model

--- jinistagram/jinistagram/images/models.py
+++ jinistagram/jinistagram/images/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+    # 최초 생성
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 새로고침
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Image(TimeStampedModel):
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    cpation = models.TextField()
+
+class Comment(TimeStampedModel):
+    message = models.TextField()
