8d5af7220cb3d1d3d4b073256209015ca81331e8
jiniC <jinic323@gmail.com>
2018-03-25 20:18:03 +0900

    Creating the Like Model & Updating Models by Adding Relationships

--- jinistagram/jinistagram/images/models.py
+++ jinistagram/jinistagram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from jinistagram.users import models as user_models # User 불러오기
 
 # Create your models here.
 class TimeStampedModel(models.Model):
@@ -11,9 +12,19 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 class Image(TimeStampedModel):
+    """ Image Model """
     file = models.ImageField()
     location = models.CharField(max_length=140)
     cpation = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
 
 class Comment(TimeStampedModel):
+    """ Comment Model """
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+class Like(TimeStampedModel):
+    """ Like Model """
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
