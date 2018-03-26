dfd41204a084b1bbe7436d01467bbf0f92960093
jiniC <jinic323@gmail.com>
2018-03-26 23:08:17 +0900

    Installing the Django RestFramework

--- jinistagram/config/settings/base.py
+++ jinistagram/config/settings/base.py
@@ -50,6 +50,7 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'rest_framework' #REST framework
 ]
 
 # Apps specific for this project go here.
