3af4429f2c99cb59877e0ac519ea010270c50411
jiniC <jinic323@gmail.com>
2018-03-31 22:29:52 +0900

    Connecting Related Models Fields

--- jinistagram/jinistagram/images/models.py
+++ jinistagram/jinistagram/images/models.py
@@ -9,18 +9,18 @@ class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     # 새로고침
     updated_at = models.DateTimeField(auto_now=True)
-
     class Meta:
         abstract = True
 
 @python_2_unicode_compatible
 class Image(TimeStampedModel):
     """ Image Model """
+    id = 1
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
-
+    # image_set = (LOOK IN ALL THE COMMENTS FOR THE ONES THAT HAVE 'IMAGE' = 1)
     def __str__(self):
         return '{} - {}'.format(self.location, self.caption)
 
@@ -29,8 +29,7 @@ class Comment(TimeStampedModel):
     """ Comment Model """
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
-    image = models.ForeignKey(Image, null=True)
-
+    image = models.ForeignKey(Image, null=True, related_name='comments')
     def __str__(self):
         return self.message
 
@@ -38,7 +37,6 @@ class Comment(TimeStampedModel):
 class Like(TimeStampedModel):
     """ Like Model """
     creator = models.ForeignKey(user_models.User, null=True)
-    image = models.ForeignKey(Image, null=True)
-
+    image = models.ForeignKey(Image, null=True, related_name='likes')
     def __str__(self):
         return 'User:{} - Image Caption{}'.format(self.creator.username, self.image.caption)
