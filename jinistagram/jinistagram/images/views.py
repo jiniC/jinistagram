b6f6a638cea70f09b7c4307877638b6ab5ad7a4d
jiniC <jinic323@gmail.com>
2018-04-01 23:18:03 +0900

    How to create a Query Statement

--- jinistagram/jinistagram/images/views.py
+++ jinistagram/jinistagram/images/views.py
@@ -10,7 +10,12 @@ class ListAllImages(APIView):
 
 class ListAllComments(APIView):
     def get(self, request, format=None):
-        all_comments = models.Comment.objects.all()
+        # print(request.user.id) 페이지 로딩하는 user id
+        # 페이지 로딩하는 user가 쓴 댓글만 (내가 쓴 댓글 보기)
+        user_id = request.user.id
+        all_comments = models.Comment.objects.filter(creator=user_id)
+        # 모든 댓글 보기
+        # all_comments = models.Comment.objects.all()
         serializer = serializers.CommentSerializer(all_comments, many=True)
         return Response(data=serializer.data)
 
