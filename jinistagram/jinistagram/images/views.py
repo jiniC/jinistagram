22eefd8b7dcef60c80e07b87f636948a67ecb9e3
jiniC <jinic323@gmail.com>
2018-04-06 07:27:36 +0900

    Liking & Unliking an Image

--- jinistagram/jinistagram/images/views.py
+++ jinistagram/jinistagram/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status # django status code
 from . import models, serializers
 
 class Feed(APIView):
@@ -20,7 +21,30 @@ class Feed(APIView):
         return Response(serializer.data)
 
 class LikeImage(APIView):
-    # 데이터베이스에서 뭐가 변하면 post요청 (현재는 임시로 get)
+    # 데이터베이스에서 뭐가 변하면(http request보낼 수 있는건) post, put요청 (현재는 임시로 get)
     def get(self, request, image_id, format=None):
-        print(image_id)
-        return Response(status=200)
+        user = request.user
+        # 이미지 찾기
+        try:
+            found_image=models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+           #  return Response(status=404)
+           return Response(status=status.HTTP_404_NOT_FOUND)
+            # return 하면 function은 종료
+        # like
+        try:
+            preexisting_like=models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexisting_like.delete()
+            # return Response(status=204) # no content
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        #unlike
+        except models.Like.DoesNotExist:
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+            new_like.save()
+            return Response(status=status.HTTP_201_CREATED)
