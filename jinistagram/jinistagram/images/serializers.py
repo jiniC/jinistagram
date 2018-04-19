510d69045bb78374e74cbf0db249231354e83e28
jiniC <jinic323@gmail.com>
2018-04-19 22:33:14 +0900

    Getting Single Photo

--- jinistagram/jinistagram/images/serializers.py
+++ jinistagram/jinistagram/images/serializers.py
@@ -58,5 +58,6 @@ class ImageSerializer(serializers.ModelSerializer):
             'caption',
             'comments',
             'like_count',
-            'creator'
+            'creator',
+            'created_at'
         )
