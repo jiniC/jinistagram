df92dcbe29bf3174e14efc54fae8d5e28087ac1e
jiniC <jinic323@gmail.com>
2018-07-26 08:02:35 +0900

    Syncing React Router with Redux

--- jinistagram/frontend/src/redux/configureStore.js
+++ jinistagram/frontend/src/redux/configureStore.js
@@ -1,13 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
 import thunk from "redux-thunk";
 import users from 'redux/modules/users';
 //import { logger } from "redux-logger"; => prod 일때도  redux-logger를 부름 (무거워짐)
 
 // process: node js 의 전체 정보 가지고 있는 변수
 const env = process.env.NODE_ENV;
-// console.log(env);
 
-const middlewares = [thunk]; // middlewares 배열
+const history = createHistory();
+const middlewares = [thunk, routerMiddleware(history)]; // middlewares 배열
 
 if(env==="development") {
     const { logger } = require("redux-logger");
@@ -16,10 +18,13 @@ if(env==="development") {
 
 // 리듀서들 합침 -> 한개의 스토어
 const reducer = combineReducers({
-    users
+    users,
+    routing: routerReducer
 });
 
 let store = initialState => createStore(reducer, applyMiddleware(...middlewares)); 
 // middlewares 만 써주면 배열 [thunk] 를 풀어서 써줌
+
+export { history };
 
 export default store();
